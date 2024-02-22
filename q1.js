36ef61169181b83951f50e53e0abf6d8adc9bbef
gokuls-16 <sgokulsg16@gmail.com>
2024-02-22 07:56:08 +0530

    first commit

---
+++ q1.js
@@ -0,0 +1,61 @@
+
+let myResume={
+    "basics": {
+      "name": "S GOKUL",
+      "email": "sgokulsg16@gmail.com",
+      "phone": 6380664290,
+      "degree": "B.E",
+      "location": {
+        "address": "71/1a,ramakrishnanagar,second street",
+        "postalCode": 603001,
+        "city": "Chengalpattu",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/gokuls-16"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "ZF Rane PVT",
+        "position": "Supervisor",
+        "startDate": "2023-11-02",
+        "summary": "i am the supervisor handle the me belowed employess",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Government College Of Engineering,salem",
+        "department": "Mechanical",
+        "studyType": "fulltime",
+        "batch start year": 2014,
+        "batch end year": 2018,
+        "gpa": 7.2,
+      }
+    ],
+    "skills": [
+      {
+        "name": "FSD",
+        "level": "beginer",
+        "project": [
+          "Created soon"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "meme creator",
+      }
+    ]
+  }
+  console.log(myResume);
+  
+  
